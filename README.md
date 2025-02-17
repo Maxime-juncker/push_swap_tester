5d901e2536e094e3b4ea1dbb81b7215298cceee0
Maxime Juncker <maxime.junck@gmail.com>
2025-02-17 12:18:03 +0100

    added log file

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # push swap tester
 
 ### features
-this tester will run push_swap `n` time then tell 
-you the max and average number of operation your program took to sort a stack of random values
+- this tester will run push_swap `n` time then tell you the max and average number of operation your program took to sort a stack of random values
+- a log file (tester.log) is created, it contain for every maximum found the test in order to reproduce the result
+
+
 
 ## how to use
 ### installation
 clone the repo in your project
 ```
-https://github.com/Maxime-juncker/push_swap_tester.git
+git clone https://github.com/Maxime-juncker/push_swap_tester.git
 ```
 
 before running the tester, you may need to changes these variables in `benchmark.sh` based on your needs
--- benchmark.sh
+++ benchmark.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-ITERATIONS=500							# number of time push swap will be tested
+ITERATIONS=100							# number of time push swap will be tested
 NB_ARGS=100								# size of the stack
 PUSH_SWAP_PATH=../push_swap				# path of push_swap executable
 
 AVRG=0
 MAX=0
+TRY=0
+
+rm tester.log
 
 printf "\n\033[0m\e[?25l---------------------------------\n"
 printf "\033[33mtesting $NB_ARGS\033[33m random numbers $ITERATIONS\033[33m times\n\n\033[0m"
 
+echo "tester settings: $NB_ARGS random numbers $ITERATIONS times" >> tester.log
+printf "=============================================\n\n" >> tester.log
+
 for i in $(seq 1 $ITERATIONS);
 do
-	COUNT=$(ARG="$(echo $(python3 rand_numbers.py $NB_ARGS))"; "$PUSH_SWAP_PATH" "$ARG" | wc -l)
+	TRY=$(python3 rand_numbers.py $NB_ARGS)
+	COUNT=$(ARG="$(echo $TRY)"; "$PUSH_SWAP_PATH" "$ARG" | wc -l)
 	AVRG=$((AVRG + COUNT))
 	if [ $COUNT -gt $MAX ]; then
 		MAX=$COUNT
 		printf "push_swap score: \e[31m$COUNT\t(new max)\e[0m\n"
+		printf "push_swap score: [$COUNT]\nnumbers: $TRY\n" >> tester.log
+		echo "----------------------------------------------" >> tester.log
 	else
 		printf "push_swap score: \e[32m$COUNT\t[$(echo "scale=2; ($i / $ITERATIONS) * 100" | bc)%%]\e[0m\r"
 	fi
@@ -31,3 +40,9 @@ AVRG=$(echo "scale=1; $AVRG/$ITERATIONS" | bc)
 printf "\033[33maverage: \033[34m$AVRG\n"
 
 printf "\033[33m---------------------------------\n\e[?25h\n"
+
+
+printf "\nsession results:\n" >> tester.log
+printf "max: $MAX\n" >> tester.log
+printf "average: $AVRG\n" >> tester.log
+printf "\n----------------------------------------------\n" >> tester.log
